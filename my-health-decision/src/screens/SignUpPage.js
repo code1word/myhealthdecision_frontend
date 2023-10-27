a0d950ec101449596de97f56d037d257354ff2ab
Dhruv Yalamanchi <dhruv.yalamanchi@outlook.com>
2023-10-27 19:23:53 -0400

    Worked on signin page

--- my-health-decision/src/screens/SignUpPage.js
+++ my-health-decision/src/screens/SignUpPage.js
@@ -29,6 +29,27 @@ function SignUpPage() {
     setLastName(e.target.value);
   };
 
+  const [month, setMonth] = useState("");
+  const [day, setDay] = useState("");
+  const [year, setYear] = useState("");
+
+  const handleMonthChange = (e) => {
+    setMonth(e.target.value);
+  };
+
+  const handleDayChange = (e) => {
+    setDay(e.target.value);
+  };
+
+  const handleYearChange = (e) => {
+    setYear(e.target.value);
+  };
+
+  const [checked1, setChecked1] = useState(false);
+  const handleCheckbox1Change = (e) => {
+    setChecked1(e.target.checked);
+  };
+
   return (
     <div className="signup_page_outer_container">
       <div className="signup_page_heading">Create an account</div>
@@ -61,21 +82,85 @@ function SignUpPage() {
       />
 
       <div className="signup_page_field_heading">Your name</div>
-      <div style={{ display: "flex", width: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          marginLeft: "40px",
+          marginRight: "40px",
+          maxWidth: "920px",
+        }}
+      >
         <input
+          className="signup_page_field"
           type="text"
-          placeholder="First Name"
+          placeholder="First"
           value={firstName}
           onChange={handleFirstNameChange}
-          style={{ marginLeft: "40px", width: "100%" }}
+          style={{ marginLeft: "0px", marginRight: "5px", flexGrow: "1" }}
         />
         <input
+          className="signup_page_field"
           type="text"
-          placeholder="Last Name"
+          placeholder="Last"
           value={lastName}
           onChange={handleLastNameChange}
+          style={{ marginRight: "0px", marginLeft: "5px", flexGrow: "1" }}
         />
       </div>
+
+      <div className="signup_page_field_heading">Date of birth</div>
+      <div
+        style={{
+          display: "flex",
+          marginLeft: "40px",
+          marginRight: "40px",
+          maxWidth: "920px",
+        }}
+      >
+        <input
+          className="signup_page_field"
+          type="numeric"
+          placeholder="MM"
+          value={month}
+          onChange={handleMonthChange}
+          style={{
+            marginLeft: "0px",
+            width: "35px",
+            marginRight: "5px",
+          }}
+        />
+        <input
+          className="signup_page_field"
+          type="numeric"
+          placeholder="DD"
+          value={day}
+          onChange={handleDayChange}
+          style={{ width: "35px", marginLeft: "5px", marginRight: "5px" }}
+        />
+        <input
+          className="signup_page_field"
+          type="numeric"
+          placeholder="YYYY"
+          value={year}
+          onChange={handleYearChange}
+          style={{ width: "70px", marginLeft: "5px" }}
+        />
+      </div>
+
+      <label
+        style={{
+          marginTop: "50px",
+          marginLeft: "40px",
+          fontSize: "1.1rem",
+        }}
+      >
+        <input
+          type="checkbox"
+          checked={checked1}
+          onChange={handleCheckbox1Change}
+        />
+        Please read and accept the Terms of Use and Privacy Policy
+      </label>
 
       <div className="signup_page_save_continue">Save and continue</div>
     </div>
