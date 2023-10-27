fabfeff706c433fcbe22664b93d20c37ebd86b9e
alyucu6 <allisonycui@gmail.com>
2023-10-27 12:00:59 -0400

    search page start

--- my-health-decision/src/screens/SearchPage.js
+++ my-health-decision/src/screens/SearchPage.js
@@ -1,8 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 import "./SearchPage.css";
 
+// dummy data to test our search on
+const data = [
+  "Colonoscopy",
+  "Knee surgery",
+  "Covid-19",
+  "MRI",
+  "Ultrasound",
+  "EKG",
+  "Emergency visit"
+];
+
 function SearchPage() {
-  return <div>Hi</div>;
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+
+  const handleChange = e => {
+    setSearchTerm(e.target.value);
+  };
+  useEffect(() => {
+    const results = data.filter(data =>
+      data.toLowerCase().includes(searchTerm)
+    );
+    setSearchResults(results);
+  }, [searchTerm]);
+
+  return (
+    <div className="searchpage_outer_container">
+      <div className="searchpage-heading">What are you looking for today?</div>
+      <br></br><br></br>
+      <div className="search-bar">
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleChange}
+        />
+        <ul>
+          {searchResults.map(item => (
+            <li>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+
+  );
 }
 
 export default SearchPage;
